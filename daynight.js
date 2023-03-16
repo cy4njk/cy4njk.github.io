7aaa09dc0adaeaf4b9bfd8a401a40693a96f93a0
cy4njk <mustafa13574@gmail.com>
2023-03-16 07:02:33 +0100

    new wave

--- daynight.js
+++ daynight.js
@@ -5,7 +5,7 @@
 if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="/css/versions4.css">');
 var d = new Date();
 var currHour = d.getHours();
-    if (currHour >= 11 && currHour <= 18) {
+    if (currHour >= 6 && currHour <= 19) {
         document.write('<link rel="stylesheet" type="text/css" href="styleday.css">');
     } else {
         document.write('<link rel="stylesheet" type="text/css" href="stylenight.css">');
